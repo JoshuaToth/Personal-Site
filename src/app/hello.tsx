73069b4ffdd60ee8806e92ee72c29de85696c070
Joshua Toth <tothy4@gmail.com>
2017-08-27 13:55:10 +1000

    eod commit

--- src/app/hello.tsx
+++ src/app/hello.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import * as About from './about/about'
+import * as About from './about/about';
+import { Grid, Row, Col } from 'react-bootstrap';
 import './hello.scss';
 
 interface IHelloProps {};
@@ -10,9 +11,17 @@ export class Hello extends React.Component<IHelloProps, IHelloState> {
   render() {
     return (
       <div>
-        <img id='displayphoto' src='../img/DisPic.png' alt='Terrible display photo'/>
-        <h1 id='titlename'>{'Joshua Toth'}</h1>
-        <About.AboutMe />
+        <Grid>
+          <Row>
+            <Col sm={3} lg={3}>
+              <img id='displayphoto' src='../img/DisPic.png' alt='Terrible display photo'/>
+              <h1 id='titlename'>{'Joshua Toth'}</h1>
+            </Col>
+            <Col sm={9} lg={9}>
+              <About.AboutMe />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
