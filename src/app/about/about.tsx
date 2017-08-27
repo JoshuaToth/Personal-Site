bdbf6f0e567477121eb9fbbe77f7b9c1c255e75f
Joshua Toth <tothy4@gmail.com>
2017-08-27 15:50:50 +1000

    Commit with some basic templating again. more styles. still looks horrible

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -4,18 +4,14 @@ import './about.scss';
 
 export const AboutMe = (props) => {
     return (
-        <div className='aboutMe' >
-            <h2>About me</h2>
+        <div className='page' >
+            <img src=''></img>
+            <h1>About me</h1>
             <p>
                 I'm a Full Stack Software Developer working in Melbourne. 
                 <br/>I love technology and building software! I'm always wanting to learn new and exciting things.
                 <br/>I'm also an active member of the community, participating in few community organisations.
             </p>    
-                <ul>
-                    <li><a href='dddmelbourne.com'>DDD Melbourne</a></li>
-                    <li><a href='http://dddbynight.dddmelbourne.com/'>DDD By Night</a></li>
-                    <li><a href='https://www.meetup.com/en-AU/Melbourne-ALT-NET/'>ALT.NET</a></li>
-                </ul>
         </div>
     );
 };
--- src/app/hello.tsx
+++ src/app/hello.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as About from './about/about';
+import * as Community from './community/community';
 import { Grid, Row, Col } from 'react-bootstrap';
 import './hello.scss';
 
@@ -11,14 +12,17 @@ export class Hello extends React.Component<IHelloProps, IHelloState> {
   render() {
     return (
       <div>
-        <Grid>
-          <Row>
-            <Col sm={3} lg={3}>
+        <Grid className='container-full'>
+          <Row >
+            <Col className='left-bar' sm={3} lg={3}>
               <img id='displayphoto' src='../img/DisPic.png' alt='Terrible display photo'/>
               <h1 id='titlename'>{'Joshua Toth'}</h1>
+              <button type='button' className='btn btn-outline-light btn-lg'>About Me <i className='fa fa-angle-right'></i></button>
+              <button type='button' className='btn btn-outline-light btn-lg'>Community<i className='fa fa-angle-right'></i></button>
             </Col>
-            <Col sm={9} lg={9}>
+            <Col className='content-column' sm={9} lg={9}>
               <About.AboutMe />
+              <Community.community />
             </Col>
           </Row>
         </Grid>
