4bd242677a475e05cab6b8250e391dfaa9de6ff5
Joshua Toth <tothy4@gmail.com>
2017-10-22 16:14:11 +1100

    add travis for Build

--- conf/webpack-test.conf.js
+++ conf/webpack-test.conf.js
@@ -20,6 +20,11 @@ module.exports = {
         loaders: [
           'ts-loader'
         ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
       }
     ]
   },
--- src/app/hello.spec.tsx
+++ src/app/hello.spec.tsx
@@ -5,7 +5,7 @@ import { Hello } from './hello';
 describe('hello component', function() {
   it('should render hello world', function() {
     const hello = TestUtils.renderIntoDocument(<Hello/>);
-    //const h1 = TestUtils.findRenderedDOMComponentWithTag(Hello, 'h1');
-    //expect(h1.textContent).toEqual('Hello world!');
+    // const h1 = TestUtils.findRenderedDOMComponentWithTag(Hello, 'h1');
+    // expect(h1.textContent).toEqual('Hello world!');
   });
 });
