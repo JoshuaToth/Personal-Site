ab7bb90b077e381807da3d1015f66f705f8fb84e
JoshuaT <joshuat@pageuppeople.com>
2017-10-24 10:33:59 +1100

    Whoop I mistaked a thing

--- src/app/articles/windows-update-1709.tsx
+++ src/app/articles/windows-update-1709.tsx
@@ -6,18 +6,18 @@ import './about.scss';
 declare function require(name: string);
 
 var content = `# IIS 10 Windows Fall Creators Update (1709): 'Cannot read \\\\\\\\?\\\\\\<EMPTY\\> configuration file'
-So you've installed Windows update 1709 and now your IIS hosted website isn't working?
+So you've installed Windows update 1709 and now your IIS hosted website isn't working? (October 2017)
 
 Common Symptoms: 
 - 503 Unavailable
 - App pool will immediately stop
-- Heaps of application errors that will say something along the line of: 
+- Windows errors stating something along the line of: '... application pool 'POOLNAME' ... Cannot read \\\\\\\\?\\\\\\<EMPTY\\> configuration file'
 
 Windows update 1709 has corrupted the temp files for IIS app pools. 
 There is a relatively simple fix: 
-* Go to C:\\inetpub\\temp\\appPools 
-You will see folders representing your app pools but they look like shortcuts and cant be navigated to
-* Delete the folders (If they cant be deleted, run in powershell: 'IISRESET /stop' and then delete)
+* Go to C:\\inetpub\\temp\\appPools
+You will see folders representing your app pools but they look like shortcuts and can't be navigated to
+* Delete the folders (If they cant be deleted, run in powershell: 'IISRESET /stop' and then delete them)
 * Run in powershell IISRESET
 * Hit your site again / Start the app pools: The temporary files should repopulate normally
 `;
