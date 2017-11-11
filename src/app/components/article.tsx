c42300c0ce8f8bc989f67090be3da3d87d439c40
Joshua Toth <tothy4@gmail.com>
2017-11-11 14:20:22 +1100

    add disqus

--- src/app/components/article.tsx
+++ src/app/components/article.tsx
@@ -34,7 +34,36 @@ export const Article = (props) => {
         <div className='article' id='about-me'>
             <ArticleHeader img={props.headerImage} background={props.background}/>
             <ArticleBody content={props.content}/>
+            <Disqus/>
             <p className='footer'><b>Joshua Toth</b> © 2017. info@joshuatoth.com</p>
         </div>
     );
 };
+
+class Disqus extends React.Component<null, null> {
+    disqus_config = function () {
+        let d = document, s = d.createElement('script');
+        s.src = 'https://joshuatoth.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', new Date().toUTCString());
+        (d.head || d.body).appendChild(s);
+
+    };
+
+    render() {
+        this.disqus_config();
+        return (
+            <div className='disqus'>
+                <Grid>
+                    <Row>
+                        <Col md={2}/>
+                        <Col md={8}>
+                            <div id='disqus_thread'></div>
+                            <noscript>Please enable JavaScript to view the <a href='https://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript> 
+                        </Col>
+                        <Col md={2}/>
+                    </Row>
+                </Grid>                      
+            </div>
+        );
+    }
+}
