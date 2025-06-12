63328a604f457663e8f5a8fe06c5839fe3f4cd7a
Joshua Toth <josh@jjt.dev>
2025-06-12 19:31:58 +1000

    deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joshuatoth.github.io',
+  base: 'Personal-Site',
   vite: {
     plugins: [tailwindcss()],
   },
