5b70d6a29dcb45e98b2ea8cb803a5aa0fae2d35f
Joshua Toth <josh@jjt.dev>
2025-06-12 19:09:02 +1000

    chore: new website!

---
+++ src/lib/variables.ts
@@ -0,0 +1,42 @@
+// Set any item to undefined to remove it from the site or to use the default value
+
+export const GLOBAL = {
+  // Site metadata
+  username: 'Joshua Toth',
+  rootUrl: 'https://jjt.dev',
+  shortDescription: 'AI Tech Leader',
+  longDescription: 'Josh is a Senior Prototyping Engineer (L6) at AWS based in Melbourne',
+
+  // Social media links
+  githubProfile: 'https://github.com/JoshuaToth',
+  linkedinProfile: 'https://www.linkedin.com/in/joshua-toth-aab7b8b0/',
+
+  // Common text names used throughout the site
+  articlesName: 'Articles',
+  projectsName: 'Projects',
+  viewAll: 'View All',
+
+  // Common descriptions used throughout the site
+  noArticles: 'No featured articles yet.',
+  noProjects: 'No featured projects yet.',
+
+  // Blog metadata
+  blogTitle: 'My Thoughts & Takes',
+  blogShortDescription: 'Practical wisdom, unfiltered thoughts, and hot takes.',
+  blogLongDescription: 'Web development, tech trends, and the occasional programming mishap.',
+
+  // Project metadata
+  projectTitle: 'Projects and Code',
+  projectShortDescription: 'A list of my web development projects and developer tools.',
+  projectLongDescription: 'All of my projects, including both frontend and full-stack applications.',
+
+  // Profile image
+  profileImage: 'pfp.png',
+
+  // Menu items
+  menu: {
+    // home: '/',
+    // projects: '/projects',
+    // blog: '/blog',
+  },
+};
